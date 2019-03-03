bd6a047ad870715e7e220b3a89537c776cf7d86a
Ashley <jal9626@gmail.com>
2019-03-03 15:18:04 -0500

    before map implement

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Button.css';
 
-function ActionButton() {
-    <div className='actionButtonsContainer'>
-        <div className=''></div>
-    </div>
+function ActionButton(props) {
+   return (
+   <div className='actionButtonContainer'>
+     <div className='clearButton'></div>
+     <div className='symbolsContainer'>
+       <div className='symbol'>
+            {/* <h1>{props.symbols.symbol}</h1> */}
+       </div>
+     </div>
+   </div>
+    );
 }
 
 export default ActionButton;
---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/Symbols.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ActionButton from "./ActionButton";
+
+const symbols [
+    {
+        symbol: '&#247'
+    },
+    {
+        symbol: 'X'
+    },
+    {
+        symbol: '-'
+    },
+    {
+        symbol: '+'
+    },
+    {
+        symbol: '='
+    }
+];
+
